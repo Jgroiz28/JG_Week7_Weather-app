858e0ec3237f22fa33dd7c2d9560ca0f622aab31
Julie Groizeleau <julie.groizeleau@gmail.com>
2023-03-23 17:21:40 -0600

    remove console logs

--- src/app.js
+++ src/app.js
@@ -26,31 +26,24 @@ function displayTemperature(response) {
   temperatureElement.innerHTML = Math.round(
     response.data.temperature.feels_like
   );
-  console.log(temperatureElement);
 
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
-  console.log(cityElement);
 
   let temperatureMinimumElement = document.querySelector("#temperatureMinimum");
   temperatureMinimumElement.innerHTML = response.data.temperature.minimum;
-  console.log(temperatureMinimumElement);
 
   let temperatureMaximumElement = document.querySelector("#temperatureMaximum");
   temperatureMaximumElement.innerHTML = response.data.temperature.maximum;
-  console.log(temperatureMaximumElement);
 
   let humidityElement = document.querySelector("#humidityElement");
   humidityElement.innerHTML = response.data.temperature.humidity;
-  console.log(humidityElement);
 
   let windElement = document.querySelector("#windElement");
   windElement.innerHTML = response.data.wind;
-  console.log(windElement);
 
   let dayTimeElement = document.querySelector("#dayTime");
   dayTimeElement.innerHTML = formatDate(response.data.time * 1000);
-  console.log(dayTimeElement);
 }
 
 let apiKey = "3ea3aa909t0c294736f1b2o85fbc5802";
